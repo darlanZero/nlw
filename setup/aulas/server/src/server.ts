d42d8aca3d7dbd97a6e8873ccdd0ca69997896ad
darlanZero <darliankeira229@gmail.com>
2023-01-29 22:24:41 -0300

    first commit

---
+++ setup/aulas/server/src/server.ts
@@ -0,0 +1,21 @@
+//Back-end API RESTful
+//
+import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { appRoutes } from "./lib/routes";
+
+const app = Fastify()
+
+
+app.register(cors)
+app.register(appRoutes)
+
+/**
+ * Método HTTP: Get, Post, Put, Patch, Delete
+ */
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP Server running!')
+})
